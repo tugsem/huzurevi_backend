7b04908859ed404a8ee4120dd5e8844c601d2875
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-03-21 18:50:14 +0300

    add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
+  config.hosts << "huzurevibackend-production.up.railway.app"
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
