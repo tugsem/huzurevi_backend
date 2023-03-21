10ed67765b81e8071b27cca2219aa38f177c7924
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-03-21 20:06:10 +0300

    fix blocked host error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,10 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  # Code is not reloaded between requests.
+
   config.hosts << "huzurevibackend-production.up.railway.app"
+
+# Code is not reloaded between requests.
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
