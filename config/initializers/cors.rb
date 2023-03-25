556aade70580999d97b6043db2b3f23a1fa77bba
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-03-25 21:54:07 +0300

    Add cors host and change puma conf

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "https://huzureviproduct.netlify.app:80"
 
     resource "*",
       headers: :any,
