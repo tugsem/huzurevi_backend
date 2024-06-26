9c7638b28ecf353ad9c37b8834b186bfe1b60350
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-06-26 17:25:31 +0300

    remove brackets

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["http://localhost:3000", "https://huzur-evi.netlify.app/"]
+    origins 'http://localhost:3000', 'https://huzur-evi.netlify.app/'
 
-    resource "*",
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
