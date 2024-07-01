a6a1b3206953e94e69b506e4c307b62aac9a10a5
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-07-01 17:42:13 +0300

    Add api to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://huzur-evi.netlify.app/'
+    origins 'http://localhost:3000', 'https://huzur-evi.netlify.app/', 'huzurevibackend-production.up.railway.app'
 
     resource '*',
       headers: :any,
