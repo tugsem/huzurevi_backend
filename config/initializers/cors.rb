2d9601006bcea60c4369b2e92c9158e4ed65e69d
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-07-16 01:01:16 +0300

    change cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'huzurevibackend-production.up.railway.app', 'http://localhost:3000', 'https://huzur-evi.netlify.app'
+    origins '*'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+
   end
 end
